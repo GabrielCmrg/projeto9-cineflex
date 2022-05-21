57ebcbcb09858e7caf681b7cae149cf79104e710
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-05-20 23:04:37 -0300

    installed react-helmet and applied global style

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import ApplicationHead from "../theme/ApplicationHead";
+import GlobalStyle from "../theme/GlobalStyle";
+
+export default function App() {
+    return (
+        <>
+        <ApplicationHead />
+        <GlobalStyle />
+        </>
+    )
+}
